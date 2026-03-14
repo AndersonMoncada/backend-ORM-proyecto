f8f04197231bef712d6a68d8033f2c065d036aad
danikpop88 <danielarestrepocanu@gmail.com>
2026-03-14 17:04:08 -0500

    Feat:Entidad accede, de la relacion muchos a muchos

---
+++ src/Entities/Accede.py
@@ -0,0 +1,18 @@
+import uuid
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from src.database.config import Base
+
+
+class Accede(Base):
+    __tablename__ = "accede"
+    id_entrada = Column(
+        UUID(as_uuid=True), ForeignKey("entrada.id_entrada"), primary_key=True
+    )
+    id_atraccion = Column(
+        UUID(as_uuid=True), ForeignKey("atraccion.id_atraccion"), primary_key=True
+    )
+
+    entrada = relationship("Entrada")
+    atraccion = relationship("Atraccion")
---
+++ src/Entities/Entrada.py
@@ -0,0 +1,24 @@
+import uuid
+from sqlalchemy import Column, DateTime, Float, ForeignKey, String, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from src.database.config import Base
+
+
+class Entrada(Base):
+    __tablename__ = "entrada"
+    id_entrada = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    codigo = Column(String(20), nullable=False, unique=True)
+    precio = Column(Float, nullable=False)
+    reingreso = Column(Boolean, nullable=False, default=False)
+    fecha = Column(DateTime(timezone=True), server_default=func.now())
+
+    id_titular = Column(
+        UUID(as_uuid=True), ForeignKey("titular.id_titular"), nullable=False
+    )
+    titular = relationship("Titular")
+
+    atracciones = relationship(
+        "Atraccion", secondary="accede", back_populates="entradas"
+    )
